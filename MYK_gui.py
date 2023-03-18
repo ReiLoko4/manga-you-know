170225d748f9a82ec8a041be938b417e0d5d949d
ReiLoko4 <103978193+ReiLoko4@users.noreply.github.com>
2023-03-18 16:34:12 -0300

    Update MYK_gui.py
    I change the size of the cards, to adapte to a size of a manga cover, and i think this look good. And I change the size of the window

--- MYK_gui.py
+++ MYK_gui.py
@@ -11,12 +11,12 @@ def window_close():
     root.iconify()
 
 root = CTk()
-root.geometry("700x460")
+root.geometry("800x550")
 root.resizable(width=False, height=False)
 root.wm_title("Manga You Know")
 root.iconbitmap(bitmap="assets/pasta_vermelha.ico")
 
-tabs = CTkTabview(master=root, width=500, height=450)
+tabs = CTkTabview(master=root, width=600, height=450)
 tabs.pack()
 tabs.add("Favoritos")
 tabs.add("Adicionar")
@@ -26,19 +26,22 @@ tabs.add("Configurações")
 # Favoritos display
 
 
-tabfav = CTkScrollableFrame(master=tabs.tab("Favoritos"), width=500, height=20000)
+tabfav = CTkScrollableFrame(master=tabs.tab("Favoritos"), width=600, height=20000)
 tabfav.pack()
 
-capa = CTkImage(Image.open("C:/Users/thiag/Downloads/onepiece.jpg"), size=(110, 170))
+capa = CTkImage(Image.open("C:/Users/thiag/Downloads/onepiece.jpg"), size=(145, 220))
+editar = CTkImage(Image.open("C:/Users/thiag/Downloads/editar.ico"), size=(15,15))
 
 space = 10
 for i in range(3):
-    card1 = CTkFrame(master=tabfav, width=160, height=220)
+    card1 = CTkFrame(master=tabfav, width=160, height=270)
     card1.pack(anchor="w", pady=10, padx=5)
     img1 = CTkLabel(master=card1, text="", image=capa, width=140, height=150)
-    img1.place(x=4, y=4)
-    button1 = CTkButton(master=card1, text="Ver capítulos", width=100)
-    button1.place(x=9, y=185)
+    img1.place(x=8, y=6)
+    button1 = CTkButton(master=card1, text="Ver capítulos", width=107)
+    button1.place(x=8, y=235)
+    buttonedit1 = CTkButton(master=card1, text="", width=15, image=editar, fg_color="white")
+    buttonedit1.place(x=123, y=235)
     card2 = CTkFrame(master=tabfav, width=160, height=220)
     card2.place(x=180, y=space)
     button2 = CTkButton(master=card2, text="Ver capítulos", width=100)
