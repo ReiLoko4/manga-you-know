dcdea7c09ce43e76507fae03e5a8a30ff7b76c45
ReiLoko4 <103978193+ReiLoko4@users.noreply.github.com>
2023-03-19 19:52:18 -0300

    Update MYK_gui.py
    this is not beatifull, but with this code I can read from my data.csv database and use this to show my manga images

--- MYK_gui.py
+++ MYK_gui.py
@@ -2,73 +2,107 @@
 
 from customtkinter import *
 from PIL import Image
-from MYK import *
+import csv
+from math import ceil
 
-set_appearance_mode("System")
-set_default_color_theme("green")
+
+set_appearance_mode('System')
+set_default_color_theme('green')
 
 def window_close():
     root.iconify()
 
 root = CTk()
-root.geometry("800x550")
+root.geometry('800x550')
 root.resizable(width=False, height=False)
-root.wm_title("Manga You Know")
-root.iconbitmap(bitmap="assets/pasta_vermelha.ico")
+root.wm_title('Manga You Know')
+root.iconbitmap(bitmap='assets/pasta_vermelha.ico')
 
 tabs = CTkTabview(master=root, width=600, height=450)
 tabs.pack()
-tabs.add("Favoritos")
-tabs.add("Adicionar")
-tabs.add("Configurações")
+tabs.add('Favoritos')
+tabs.add('Adicionar')
+tabs.add('Configurações')
 
 
 # Favoritos display
 
+with open('data.csv', mode='r') as data_csv:
+    leitor_csv = csv.reader(data_csv)
+    data = []
+    for linha in leitor_csv:
+        data.append(linha)
+del[data[0]]
 
-tabfav = CTkScrollableFrame(master=tabs.tab("Favoritos"), width=600, height=20000)
+tabfav = CTkScrollableFrame(master=tabs.tab('Favoritos'), width=600, height=20000)
 tabfav.pack()
 
-capa = CTkImage(Image.open("C:/Users/thiag/Downloads/onepiece.jpg"), size=(145, 220))
-editar = CTkImage(Image.open("C:/Users/thiag/Downloads/editar.ico"), size=(15,15))
+editar = CTkImage(Image.open('C:/Users/ReiLoko4/Downloads/editar.ico'), size=(15,15))
 
 space = 10
-for i in range(3):
+count = 0
+for i in range(ceil((len(data)) /3)):
     card1 = CTkFrame(master=tabfav, width=160, height=270)
-    card1.pack(anchor="w", pady=10, padx=5)
-    img1 = CTkLabel(master=card1, text="", image=capa, width=140, height=150)
+    card1.pack(anchor='w', pady=10, padx=5)
+    capa1 = CTkImage(Image.open(data[count][4]), size=(145, 220))
+    img1 = CTkLabel(master=card1, text='', image=capa1, width=140, height=150)
     img1.place(x=8, y=6)
-    button1 = CTkButton(master=card1, text="Ver capítulos", width=107)
+    button1 = CTkButton(master=card1, text='Ver capítulos', width=107)
     button1.place(x=8, y=235)
-    buttonedit1 = CTkButton(master=card1, text="", width=15, image=editar, fg_color="white")
+    buttonedit1 = CTkButton(master=card1, text='', width=15, image=editar, fg_color='white')
     buttonedit1.place(x=123, y=235)
-    card2 = CTkFrame(master=tabfav, width=160, height=220)
-    card2.place(x=180, y=space)
-    button2 = CTkButton(master=card2, text="Ver capítulos", width=100)
-    button2.place(x=9, y=185)
-    card3 = CTkFrame(master=tabfav, width=160, height=220)
-    card3.place(x=355,y=space)
-    button3 = CTkButton(master=card3, text="Ver capítulos", width=100)
-    button3.place(x=9, y=185)
-    space+=240
 
+    count+=1
+    try:
+        if str(count+1) in data[count][0]:
+            card2 = CTkFrame(master=tabfav, width=160, height=270)
+            card2.place(x=180, y=space)
+            capa2 = CTkImage(Image.open(data[count][4]), size=(145, 220))
+            img2 = CTkLabel(master=card2, text='', image=capa2, width=140, height=150)
+            img2.place(x=8, y=6)
+            button2 = CTkButton(master=card2, text='Ver capítulos', width=107)
+            button2.place(x=8, y=235)
+            buttonedit2 = CTkButton(master=card2, text='', width=15, image=editar, fg_color='white')
+            buttonedit2.place(x=123, y=235)
+
+
+            count+=1
+            try:
+                if str(count + 1) in data[count][0]:
+                    card3 = CTkFrame(master=tabfav, width=160, height=270)
+                    card3.place(x=355,y=space)
+                    capa3 = CTkImage(Image.open(data[count][4]), size=(145, 220))
+                    img3 = CTkLabel(master=card3, text='', image=capa3, width=140, height=150)
+                    img3.place(x=8, y=6)
+                    button3 = CTkButton(master=card3, text='Ver capítulos', width=100)
+                    button3.place(x=8, y=235)
+                    buttonedit3 = CTkButton(master=card3, text='', width=15, image=editar, fg_color='white')
+                    buttonedit3.place(x=123, y=235)
+                    count+=1
+            except Exception:
+                break
+    except Exception:
+        break            
+    space+=290
+
+btn = CTkButton(master=tabfav, text='sumir', command=window_close)
+btn.pack()
 
 # Adicionar display
 
 
-new_fav = CTkEntry(master=tabs.tab("Adicionar"), placeholder_text="https://mangalivre.net/genero/nomeDoManga/idDoManga", width=330)
+new_fav = CTkEntry(master=tabs.tab('Adicionar'), placeholder_text='https://mangalivre.net/genero/nomeDoManga/idDoManga', width=330)
 new_fav.pack(pady=13,padx=13)
 
 
 # Configurações display
 
 
-always_donwload = CTkCheckBox(master=tabs.tab("Configurações"), text="Sempre baixar novos capítulos")
+always_donwload = CTkCheckBox(master=tabs.tab('Configurações'), text='Sempre baixar novos capítulos')
 always_donwload.pack(pady=13, padx=13)
-save = CTkButton(master=tabs.tab("Configurações"), text="Salvar")
+save = CTkButton(master=tabs.tab('Configurações'), text='Salvar')
 save.pack(pady=13, padx=13)
 
-btn = CTkButton(master=tabfav, text="sumir", command=window_close)
-btn.pack()
+
 
 root.mainloop()
