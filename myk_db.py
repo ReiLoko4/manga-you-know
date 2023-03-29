31bf85fdcb2a20018e56549aa809a8a831e74637
ReiLoko4 <103978193+ReiLoko4@users.noreply.github.com>
2023-03-28 23:23:08 -0300

    Update myk_db.py

--- myk_db.py
+++ myk_db.py
@@ -8,20 +8,57 @@ class MangaYouKnowDB:
         self.config = Path('database/config.json')
 
     def create_database(self):
-        self.database.mkdir(parents=True, exist_ok=True)
-        with open(self.database, mode='w') as data_csv:
-            writer_csv = csv.writer(data_csv)
-            # bla bla bla tem q terminar
+        if not self.database.exists():
+            self.database.touch()
+            with open(self.database, mode='w') as file:
+                writer_csv = csv.writer(file, lineterminator='\n')
+                writer_csv.writerow([
+                    'id_manga',
+                    'name_manga',
+                    'type',
+                    'manga_path'
+                ])
 
-    def get_database(self):
+    def get_database(self) -> list:
         self.create_database()
-        with open(self.database, mode='r') as data_csv:
-            leitor_csv = csv.reader(data_csv)
+        with open(self.database, mode='r') as file:
+            leitor_csv = csv.reader(file)
             database = []
             for linha in leitor_csv:
                 database.append(linha)
         del[database[0]]
         return database
-    
+
     def create_config(self):
-        self.config.mkdir(parents=True, exist_ok=True)
+        if not self.config.exists():
+            self.config.touch()
+            with open(self.config, mode='w') as file:
+                json.dump({
+                    'config': {
+                    'reader-type':'pass-n',
+                    'theme-color':'blue'
+                    }
+                }, file)
+
+    def get_config(self) -> dict:
+        self.create_config()
+        with open(self.config, mode='r') as file:
+            config = json.load(file)
+        return config
+
+    def add_manga(self, manga:list):
+        self.create_database()
+        with open(self.database, mode='a') as file:
+            writer_csv = csv.writer(file, lineterminator='\n')
+            writer_csv.writerow(manga)
+    
+    def del_manga(self, manga_id:str):
+        with open(self.database, "r") as file:
+            leitor_csv = csv.reader(file)
+            lista_csv = list(leitor_csv)
+        for linha in lista_csv:
+            if manga_id in linha:
+                lista_csv.remove(linha)
+        with open(self.database, "w", newline="") as arquivo_csv:
+            escritor_csv = csv.writer(arquivo_csv)
+            escritor_csv.writerows(lista_csv)
