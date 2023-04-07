a4ce977df210e21532864b55f09b0c53ae39d41b
ReiLoko4 <103978193+ReiLoko4@users.noreply.github.com>
2023-04-06 23:50:35 -0300

    Update myk_db.py
    - now can't have two chapters with the same id in the manga list, but its can have the same name.

--- myk_db.py
+++ myk_db.py
@@ -25,8 +25,8 @@ class MangaYouKnowDB:
         with open(self.database, mode='r', encoding='utf-8') as file:
             leitor_csv = csv.reader(file)
             database = []
-            for linha in leitor_csv:
-                database.append(linha)
+            for line in leitor_csv:
+                database.append(line)
         del[database[0]]
         return database
 
@@ -53,25 +53,40 @@ class MangaYouKnowDB:
             writer_csv = csv.writer(file, lineterminator='\n')
             writer_csv.writerow(manga)
     
-    def add_data_chapters_txt(self,chapters_list:list, manga_name:str):
+    def add_data_chapters(self, manga_name:str, chapters_list:list):
         manga_data_path = Path(f'Mangas/{manga_name}/data/')
         manga_data_path.mkdir(parents=True, exist_ok=True)
-        data_file = Path(f'{manga_data_path}/{manga_name}.txt')
+        data_file = Path(f'{manga_data_path}/chapters.csv')
         data_file.touch(exist_ok=True)
-        with open(data_file, mode='a+', encoding='utf-8') as file:
-            file.write(str(chapters_list))
-        # for chapter in chapters_list:
-        #     with open(data_file, mode='a+', encoding='utf-8') as file:
-        #         file.write(f'{chapter} ')
+        for chapter in chapters_list:
+            with open(data_file, mode='a+') as file:
+                writer_csv = csv.writer(file, lineterminator='\n')
+                writer_csv.writerow(chapter)
+
+    def get_data_chapters(self, manga_name:str) -> list or bool :
+        manga_chapters = Path(f'Mangas/{manga_name}/data/chapters.csv')
+        if not manga_chapters.exists(): return False
+        with open(manga_chapters, mode='r', encoding='utf-8') as file:
+            leitor_csv = csv.reader(file)
+            list_chapters = []
+            for line in leitor_csv:
+                list_chapters.append(line)
+        return list_chapters
+    
+    def get_chapter_id(self, manga_name:str, chapter:str) -> str or bool:
+        chapters = self.get_data_chapters(manga_name)
+        for line in chapters:
+            if chapter == line[0]: return line[1]
+
 
     def delete_manga(self, manga_id:str):
         manga_id = str(manga_id)
         with open(self.database, "r") as file:
             leitor_csv = csv.reader(file)
             lista_csv = list(leitor_csv)
-        for linha in lista_csv:
-            if manga_id in linha:
-                lista_csv.remove(linha)
+        for line in lista_csv:
+            if manga_id in line:
+                lista_csv.remove(line)
         with open(self.database, "w", newline="") as arquivo_csv:
             escritor_csv = csv.writer(arquivo_csv)
             escritor_csv.writerows(lista_csv)
