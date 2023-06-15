65e92ce111cf1d7bbd341c982015750ed007b75b
ReiLoko4 <103978193+ReiLoko4@users.noreply.github.com>
2023-06-14 21:21:03 -0300

    Add the default structure

--- MangaYouKnow/screen/configs.py
+++ MangaYouKnow/screen/configs.py
@@ -1,5 +1,12 @@
 import flet as ft
 
 
-def Configs(page):
-    pass
+def Configs(page: ft.Page):
+    content = ft.Row(
+        [
+            ft.Text('Configs')
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
+    )
+
+    return content
