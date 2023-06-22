d9f6937b2d41935deea4704a015c8187d87b8a3c
ReiLoko4 <103978193+ReiLoko4@users.noreply.github.com>
2023-06-21 23:51:36 -0300

    Added page to add manga from search

--- MangaYouKnow/screen/index.py
+++ MangaYouKnow/screen/index.py
@@ -1,16 +1,69 @@
 import flet as ft
+from time import sleep
 from backend.downloader import MangaLivreDl
+from backend.database import DataBase
 
 
 def Index(page: ft.Page):
 
-
-    results = ft.Column(width=500, spacing=0.2)
+    database = DataBase()
+    results = ft.Column(width=500, spacing=0.7)
     card = ft.Card(ft.Container(results), color='gray', visible=False)
-    search = ft.TextField(label='Pesquisar mangás...', width=500, border_radius=20)
+    search = ft.TextField(
+        label='Pesquisar mangás...', 
+        width=500, 
+        border_radius=20, 
+        border_color=ft.colors.GREY_700,
+        focused_border_color= ft.colors.BLUE_300
+    )
     downloader = MangaLivreDl()
 
-    def do_something(info_manga):
+    index = ft.Stack(width=1000, height=1000)
+    manga = ft.Row(visible=False)
+    def back_index(e):
+        manga.visible = False
+        index.visible = True
+        page.update()
+    
+    
+
+    def manga_page(info_manga):
+        index.visible = False
+        manga.controls.clear()
+        def favorite_manga(e):
+            database.add_manga({
+                'id':info_manga['id_serie'],
+                'name':info_manga['name'],
+                'folder_name': info_manga['link'].split('/')[-2]
+            })
+
+        container_img = ft.Container(padding=20, bgcolor=ft.colors.GREY_900)
+        manga.controls.append(ft.Container(
+            ft.Column([
+                ft.Row([
+                    ft.Container(
+                    ft.Row([
+                        ft.Container(
+                            ft.IconButton(ft.icons.ARROW_BACK_IOS_ROUNDED, on_click=back_index, width=50, height=50),
+                            padding=5,
+                        ),
+                        ft.Container(
+                            ft.Text(info_manga['name'], size=23),
+                            padding=10,
+                            width=1200,
+                            height=60
+                        )
+                    ], alignment=ft.CrossAxisAlignment.CENTER
+                    ), bgcolor=ft.colors.GREY_900)
+                ]),
+                ft.Row([
+                    container_img,
+                    ft.TextButton('Favoritar mangá', on_click=favorite_manga)
+        ])])))
+        manga.visible = True
+        page.update()
+        container_img.content = ft.Image(src=info_manga['cover'], height=400, width=ft.ImageFit.FIT_HEIGHT)
+        page.update()
         print(info_manga)
 
     def search_mangas(e:ft.ControlEvent):
@@ -27,6 +80,7 @@ def Index(page: ft.Page):
         if not response:
             results.controls.append(
                 ft.ListTile(
+                    key='noresult',
                     title=ft.Text('Nenhum mangá encontrado!'),
                     disabled=True,
                     height=40
@@ -36,25 +90,44 @@ def Index(page: ft.Page):
             for manga in response:
                 results.controls.append(
                     ft.ListTile(
+                        key='manga',
                         title=ft.Text(manga['name']),
                         height=45 if len(manga['name']) < 60 else 55,
-                        on_click=lambda e, info=manga: do_something(info)
+                        on_click=lambda e, info=manga: manga_page(info)
                     )
                 )
             if len(search.value) == 0:
                 results.controls.clear()
                 card.visible = False
             page.update()
-        
+    def out_search(e):
+        sleep(0.1)
+        card.visible = False
+        page.update()
+    def focus_search(e):
+        if len(results.controls) != 0:
+            if results.controls[0].key == 'manga':
+                card.visible = True
+                page.update()
     search.on_change = search_mangas
+    search.on_blur = out_search
+    search.on_focus = focus_search
 
+    index.controls.append(
+        ft.Row([ft.Container(search, padding=10)])
+    )
+    index.controls.append(
+        ft.Row([ft.Text('filler', color='black')], top=100)
+    )
+    index.controls.append(
+        ft.Row([card], top=65, left=5)
+    )
+    
     content = ft.Row(
         [   
             ft.Stack([
-                ft.Row([ft.Container(search, padding=10)]),
-                # all content in the row bellow 
-                ft.Row([ft.Text('filler', color='black')], top=100),
-                ft.Row([card], top=65, left=10)
+                index,
+                manga
             ], width=1000, height=1000)
         ],
     )
