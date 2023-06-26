5dbec9f84e679d69316945236b4678464df20658
ReiLoko4 <103978193+ReiLoko4@users.noreply.github.com>
2023-06-25 22:29:22 -0300

    Update favorites.py

--- MangaYouKnow/screen/favorites.py
+++ MangaYouKnow/screen/favorites.py
@@ -1,13 +1,29 @@
 import flet as ft
-
+from backend.database import DataBase
 
 def Favorites(page: ft.Page):
+    database = DataBase()
+    search = ft.TextField(
+        label='Pesquisar Favoritos...', 
+        width=500, 
+        border_radius=20, 
+        border_color=ft.colors.GREY_700,
+        focused_border_color= ft.colors.BLUE_300
+    ) 
+
+    card = ft.Card()
+
 
     content = ft.Row(
-        [
-            ft.Text('Favorites')
+        [   
+            ft.Stack([
+                ft.Row([ft.Container(search, padding=10)], alignment=ft.MainAxisAlignment.CENTER),
+                ft.Divider(height=170, color='white'),
+                ft.Row([card])
+            ],
+            width=1000,
+            height=1000)
         ],
-        alignment=ft.MainAxisAlignment.CENTER
     )
 
     return content
