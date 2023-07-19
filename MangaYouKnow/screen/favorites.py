0b5421f443b7b88a902aa76372ce4a3bb6b4a7a7
ReiLoko4 <103978193+ReiLoko4@users.noreply.github.com>
2023-07-18 22:55:55 -0300

    Update favorites.py

--- MangaYouKnow/screen/favorites.py
+++ MangaYouKnow/screen/favorites.py
@@ -10,7 +10,7 @@ def Favorites(page: ft.Page):
         border_color=ft.colors.GREY_700,
         focused_border_color= ft.colors.BLUE_300
     ) 
-    favorites = database.get_database()['data']
+    favorites = database.get_database()
     row_mangas = ft.Row([
         ft.Card(ft.Column([
             ft.Text(i['name']),
@@ -19,34 +19,33 @@ def Favorites(page: ft.Page):
             alignment=ft.CrossAxisAlignment.CENTER
         ),
             height=350,
-            width=230,
+            width=210,
         )
         for i in favorites
     ],
         wrap=True,
-        width=page.width - 100,
+        width=page.width - 90,
         top=100,
         scroll=ft.ScrollMode.ALWAYS
     )
-    def resize(e:ft.ControlEvent):
-        row_mangas.width = float(e.control.width) - 100
-        row_mangas.update()
-        stack.width = float(e.control.width) - 100
-        stack.update()
-    page.on_resize = resize
+    # def resize(e:ft.ControlEvent):
+    #     row_mangas.width = float(e.control.width) - 100
+    #     row_mangas.update()
+    #     stack.width = float(e.control.width) - 100
+    #     stack.update()
+    # page.on_resize = resize
     stack = ft.Stack([
         ft.Row([ft.Container(search, padding=10)], alignment=ft.MainAxisAlignment.CENTER),
         ft.Divider(height=170, color='white'),
         row_mangas
     ],
-        width=page.width - 100,
+        width=page.width - 90,
         height=1500
     )
     content = ft.Row(
         [   
             stack
         ],
-        scroll=ft.ScrollMode.ALWAYS
     )
 
     return content
