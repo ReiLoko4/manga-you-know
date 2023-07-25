84dd938804b062abbb83289c6765c22b238f332f
ReiLoko4 <103978193+ReiLoko4@users.noreply.github.com>
2023-07-24 23:44:44 -0300

    Update favorites.py

--- MangaYouKnow/screen/favorites.py
+++ MangaYouKnow/screen/favorites.py
@@ -12,19 +12,31 @@ class Favorites:
             border_color=ft.colors.GREY_700,
             focused_border_color= ft.colors.BLUE_300
         ) 
-        favorites = database.get_database()
-        row_mangas = ft.Row([
-            ft.Card(ft.Column([
-                ft.Text(i['name']),
-                ft.IconButton(ft.icons.READ_MORE)
-            ],
-                alignment=ft.CrossAxisAlignment.CENTER
-            ),
-                height=350,
-                width=210,
-            )
-            for i in favorites
-        ],
+        def open(info):
+            print(info['name'])
+            alert = ft.AlertDialog(title=ft.Text("Hello, you!", height=3000))
+            page.dialog = alert
+            alert.open = True
+            page.update()
+        
+        def load_mangas() -> list[ft.Card]:
+            favorites = database.get_database()
+            return [
+                ft.Card(ft.Column([
+                    ft.Text(i['name']),
+                    ft.Image(i['cover'], height=200, fit=ft.ImageFit.FIT_HEIGHT),
+                    ft.IconButton(ft.icons.READ_MORE, on_click=lambda e, info=i:open(info))
+                ],
+                    alignment=ft.CrossAxisAlignment.CENTER
+                ),
+                    height=350,
+                    width=210,
+                )
+                for i in favorites
+            ]
+        
+        row_mangas = ft.Row(
+            load_mangas(),
             wrap=True,
             width=page.width - 90,
             top=100,
@@ -36,31 +48,21 @@ class Favorites:
         #     stack.width = float(e.control.width) - 100
         #     stack.update()
         # page.on_resize = resize
-        
+        favorites = database.get_database()
         stack = ft.Stack([
             ft.Row([ft.Container(search, padding=10)], alignment=ft.MainAxisAlignment.CENTER),
             ft.Divider(height=170, color='white'),
             row_mangas
         ],
             width=page.width - 90,
-            height=2000
+            height= len(favorites) * 360
         )
         def update(e):
             row_mangas.width = e
             stack.width = e
             favorites = database.get_database()
-            row_mangas.controls = [
-                ft.Card(ft.Column([
-                    ft.Text(i['name']),
-                    ft.IconButton(ft.icons.READ_MORE)
-                ],
-                    alignment=ft.CrossAxisAlignment.CENTER
-                ),
-                    height=350,
-                    width=210,
-                )
-                for i in favorites
-            ]
+            stack.height = len(favorites) * 360
+            row_mangas.controls = load_mangas()
             page.update()
 
         def resize(e):
