4ad4df030c93942c16893095c7f9ee66230b5931
ReiLoko4 <103978193+ReiLoko4@users.noreply.github.com>
2023-08-03 02:56:15 -0300

    Now reader works! But not too good...

--- MangaYouKnow/screen/reader.py
+++ MangaYouKnow/screen/reader.py
@@ -1,4 +1,6 @@
 import flet as ft
+import base64
+import requests
 from backend.downloader.mangalivre import MangaLivreDl
 
 
@@ -6,11 +8,54 @@ from backend.downloader.mangalivre import MangaLivreDl
 class MangaReader:
     def __init__(self, page: ft.Page):
         self.page = page
-        self.dl = MangaLivreDl()
-        self.pages = self.dl.get_manga_chapter_imgs(page.data['id_chapter'])
-        self.images = [ft.Image(i, fit=ft.ImageFit.FIT_WIDTH, height=page.height) for i in self.pages]
-        self.content = ft.Container(
+        self.pages = page.data['chapter_images']
+        self.images_b64 = []
+        for image in self.pages:
+            response = requests.get(image['legacy'])
+            self.images_b64.append(base64.b64encode(response.content).decode('utf-8'))
+        self.images = [ft.Image(src_base64=i, fit=ft.ImageFit.FIT_HEIGHT, height=page.height) for i in self.images_b64]
+        self.content = ft.Row([
             ft.Stack(
-                self.images    
-            )
+                self.images
+            )],
+            alignment=ft.MainAxisAlignment.CENTER
         )
+        page.banner.visible = False
+        for i, img in enumerate(self.images):
+            if i != 0:
+                img.visible = False
+        def next(e=None):
+            for i, img in enumerate(self.images):
+                if img.visible:
+                    if len(self.images) > i+1:
+                        img.visible = False
+                        self.images[i+1].visible = True
+                    break
+            page.update()
+        def back(e=None):
+            for i, img in enumerate(self.images):
+                if img.visible:
+                    if i != 0:
+                        img.visible = False
+                        self.images[i-1].visible = True
+                    break
+            page.update()
+        def on_key(e: ft.KeyboardEvent):
+            if e.key == 'Arrow Right':
+                next()
+            if e.key == 'Arrow Left':
+                back()
+            if e.key == 'F11':
+                if page.window_full_screen:
+                    page.window_full_screen = False
+                else:
+                    page.window_full_screen = True
+            page.update()
+        page.on_keyboard_event = on_key
+        
+    def return_content(self):
+        return self.content
+
+
+                
+            
