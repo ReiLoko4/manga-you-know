e7aa2da8ccbf9b5cbf8b314d91c33609ef2438f1
ReiLoko4 <103978193+ReiLoko4@users.noreply.github.com>
2023-08-08 01:50:50 -0300

    Add a index to see what page you are

--- MangaYouKnow/screen/reader.py
+++ MangaYouKnow/screen/reader.py
@@ -1,7 +1,9 @@
 import flet as ft
 import base64
 import requests
-from backend.downloader.mangalivre import MangaLivreDl
+from threading import Thread
+from backend.thread_manager import ThreadManager
+# from backend.downloader.mangalivre import MangaLivreDl
 
 
 
@@ -10,16 +12,31 @@ class MangaReader:
         self.page = page
         self.pages = page.data['chapter_images']
         self.images_b64 = []
-        for image in self.pages:
-            response = requests.get(image['legacy'])
-            self.images_b64.append(base64.b64encode(response.content).decode('utf-8'))
+
+        def get_base_64_image(url, index:int):
+            response = requests.get(url)
+            self.images_b64.insert(index, base64.b64encode(response.content).decode('utf-8'))
+        threads = ThreadManager()
+        for i, image in enumerate(self.pages):
+            threads.add_thread(
+                Thread(
+                    target=lambda url=image['legacy'], index=i:get_base_64_image(url, index)
+                )
+            )
+        threads.start()
+        threads.join()
+        self.currently_page = ft.Text(f' 1/{len(self.images_b64)}')
         self.images = [ft.Image(src_base64=i, fit=ft.ImageFit.FIT_HEIGHT, height=page.height) for i in self.images_b64]
-        self.content = ft.Row([
-            ft.Stack(
-                self.images
-            )],
-            alignment=ft.MainAxisAlignment.CENTER
-        )
+        self.content = ft.Stack([
+            ft.Row(
+                self.images, 
+                alignment=ft.MainAxisAlignment.CENTER
+            ),
+            ft.Row(
+                [ft.Card(ft.Row([self.currently_page], alignment=ft.MainAxisAlignment.CENTER), height=30, width=50, opacity=0.5), ft.Container(width=10)],
+                alignment=ft.MainAxisAlignment.END,
+            )
+        ])
         page.banner.visible = False
         for i, img in enumerate(self.images):
             if i != 0:
@@ -31,6 +48,7 @@ class MangaReader:
                         img.visible = False
                         self.images[i+1].visible = True
                         self.images[i+1].height = self.page.height
+                        self.currently_page.value = f'{i+2}/{len(self.images)}'
                     break
             page.update()
         def back(e=None):
@@ -39,6 +57,7 @@ class MangaReader:
                     if i != 0:
                         img.visible = False
                         self.images[i-1].visible = True
+                        self.currently_page.value = f'{i}/{len(self.images)}'
                     break
             page.update()
         def on_key(e: ft.KeyboardEvent):
